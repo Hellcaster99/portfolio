5f0074df0956240fccc5bd1c9f5182741b85c35d
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-03-13 00:57:36 +0530

    exp section changes

--- src/ui/Project.jsx
+++ src/ui/Project.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import {motion as m} from 'framer-motion';
 import styles from '@/styles/Project.module.css';
 
-export default function index({index, title,category, setModal}) {
+export default function Project({index, title,category, setModal}) {
 
     return (
         <m.div initial={{opacity:0,y:"40%"}} whileInView={{opacity:1,y:0,transition:{duration:1.5,type:"spring",delay:0.1}}} onMouseEnter={() => {setModal({active: true, index})}} onMouseLeave={() => {setModal({active: false, index})}} className={styles.project}>
