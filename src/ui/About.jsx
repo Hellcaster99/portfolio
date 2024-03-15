894db86c4df7995440d5990de208c11255fcd859
Chinmay Pandya <chinmaypandya043@gmail.com>
2024-03-16 01:50:25 +0530

    mask changes

--- src/ui/About.jsx
+++ src/ui/About.jsx
@@ -7,9 +7,9 @@ import { Outfit } from 'next/font/google';
 const font = Outfit({subsets:['latin'],weight:['400']});
 
 const cursorVariants = {
-    initial: {scale: 0, x:"-50%", y:"-830%"},
-    enter: {scale: 1, x:"-50%", y:"-830%", transition: {duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
-    closed: {scale: 0, x:"-50%", y:"-830%", transition: {duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
+    initial: {scale: 0, x:"-50%", y:"-440%"},
+    enter: {scale: 1, x:"-50%", y:"-440%", transition: {duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
+    closed: {scale: 0, x:"-50%", y:"-440%", transition: {duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
 }
 
 export default function About(){
--- src/ui/Exp.jsx
+++ src/ui/Exp.jsx
@@ -41,9 +41,9 @@ export default function Exp() {
   return (
     <>
     <div id="exp" className={styles.container}>
-        <div className={styles.h1box} initial={{opacity:0,y:"40%"}} whileInView={{opacity:1,y:0,transition:{duration:1.5,type:"spring",delay:0.1}}}>
+        <m.div className={styles.h1box} initial={{opacity:0,y:"40%"}} whileInView={{opacity:1,y:0,transition:{duration:1.5,type:"spring",delay:0.1}}}>
         <m.p className={styles.h1} >Recent Work and Projects</m.p>
-        </div>
+        </m.div>
         <h2 className={styles.click}>Click on each for effect</h2>
         <div className={styles.ds}>
             <div className={styles.row}>
